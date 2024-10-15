741cb5991a32cf6e705aed29125f162699f88786
chyo1 <hyowon3856@gmail.com>
2024-10-15 19:23:17 +0900

    [BE] FEAT : oauth login 함수 분리

--- backend/backend/views.py
+++ backend/backend/views.py
@@ -15,7 +15,6 @@ from django.http import JsonResponse
 from django.shortcuts import render
 from django.core.mail import BadHeaderError
 from smtplib import SMTPException
-from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 
 
@@ -27,94 +26,111 @@ def home_view(request):
 def login_view(request):
   # OAuth 로그인 시작: 첫 요청
   if 'code' not in request.GET:
-    # 인증 서버로 보낼 URL 구성
-    oauth_url = settings.OAUTH_URI
-    params = {
-      'response_type': 'code',  # authorization code 요청
-      'client_id': settings.CLIENT_ID,  # 클라이언트 ID
-      'redirect_uri': settings.LOGIN_REDIRECT_URL,  # 리다이렉트 URI
-      'scope': 'public',  # 요청할 범위
-    }
-
-    # 인증 서버로 리다이렉트
-    auth_url = f"{oauth_url}?{urlencode(params)}"
-    return redirect(auth_url)
+    return start_ouath(request)
 
   # OAuth 인증 후 돌아온 요청 (code가 포함된 경우)
   else:
-    # 인증 서버에서 전달된 authorization code를 받음
-    authorization_code = request.GET.get('code')
+    return complete_oauth(request)
 
-    # 받은 authorization code로 access token을 요청
-    token_url = settings.TOKEN_URI
-    data = {
-      'grant_type': 'authorization_code',
-      'code': authorization_code,
-      'redirect_uri': settings.LOGIN_REDIRECT_URL,
-      'client_id': settings.CLIENT_ID,
-      'client_secret': settings.CLIENT_SECRET,
-    }
+def start_ouath(request):
+  # 인증 서버로 보낼 URL 구성
+  oauth_url = settings.OAUTH_URI
+  params = {
+    'response_type': 'code',  # authorization code 요청
+    'client_id': settings.CLIENT_ID,  # 클라이언트 ID
+    'redirect_uri': settings.LOGIN_REDIRECT_URL,  # 리다이렉트 URI
+    'scope': 'public',  # 요청할 범위
+  }
 
-    # POST 요청으로 access token을 받아옴
-    response = requests.post(token_url, data=data)
-    token_data = response.json()
+  # 인증 서버로 리다이렉트
+  auth_url = f"{oauth_url}?{urlencode(params)}"
+  return redirect(auth_url)
 
-    # 응답에서 access token을 확인
-    if 'access_token' in token_data:
-      access_token = token_data['access_token']
+def complete_oauth(request):
+  # 인증 서버에서 전달된 authorization code를 받음
+  authorization_code = request.GET.get('code')
+  access_token = request_access_token(authorization_code)
 
-      # 사용자 정보를 요청하는 API URL
-      user_info_url = settings.USER_INFO_URL
-      headers = {
-        'Authorization': f'Bearer {access_token}'  # Bearer token 방식으로 인증
+  # 응답에서 access token을 확인
+  if access_token:
+    user_info = request_user_info(access_token)
+    user, created = get_or_create_user_object(user_info)
+
+    try:
+      # UserProfile 객체 찾기
+      user_profile = UserProfile.objects.get(user=user)
+      return update_user_info(user_profile, access_token)
+
+    # UserProfile이 없을 경우 2단계 인증 진행
+    except UserProfile.DoesNotExist:
+      save_user_info_to_sessioin(request, user, access_token)
+      return login_2fa(request, user)  # 2단계 인증 시작
+
+  else:
+    return JsonResponse({'error': 'Failed to retrieve access token'},
+                        status=400)
+
+def request_access_token(authorization_code):
+  # 받은 authorization code로 access token을 요청
+  token_url = settings.TOKEN_URI
+  data = {
+    'grant_type': 'authorization_code',
+    'code': authorization_code,
+    'redirect_uri': settings.LOGIN_REDIRECT_URL,
+    'client_id': settings.CLIENT_ID,
+    'client_secret': settings.CLIENT_SECRET,
+  }
+
+  # POST 요청으로 access token을 받아옴
+  response = requests.post(token_url, data=data)
+  token_data = response.json()
+  return token_data['access_token']
+
+def request_user_info(access_token):
+  # 사용자 정보를 요청하는 API URL
+  user_info_url = settings.USER_INFO_URL
+  headers = {
+    'Authorization': f'Bearer {access_token}'  # Bearer token 방식으로 인증
+  }
+
+  # 사용자 정보 요청
+  user_info_response = requests.get(user_info_url, headers=headers)
+  return user_info_response.json()
+
+def get_or_create_user_object(user_info):
+  # User 객체 찾기 또는 생성
+  return User.objects.get_or_create(
+      username=user_info['login'],  # OAuth에서 받은 username
+      defaults={
+        'first_name': user_info['first_name'],
+        'last_name': user_info['last_name'],
+        'email': user_info['email'],
+        'username': user_info['login'],
       }
+  )
 
-      # 사용자 정보 요청
-      user_info_response = requests.get(user_info_url, headers=headers)
-      user_info = user_info_response.json()
+def update_user_info(user_profile, access_token):
+  # UserProfile이 존재할 경우 access_token 업데이트
+  user_profile.access_token = access_token
+  user_profile.save()  # 변경 사항 저장
 
-      # User 객체 찾기 또는 생성
-      user, created = User.objects.get_or_create(
-          username=user_info['login'],  # OAuth에서 받은 username
-          defaults={
-            'first_name': user_info['first_name'],
-            'last_name': user_info['last_name'],
-            'email': user_info['email'],
-            'username': user_info['login'],
-          }
-      )
+  return JsonResponse({
+    'user': {
+      'username': user_profile.user.username,
+      'email': user_profile.user.email,
+      'first_name': user_profile.user.first_name,
+      'last_name': user_profile.user.last_name,  # API에서 받아온 사용자 이름
+    }
+  })
 
-      try:
-        # UserProfile 객체 찾기
-        user_profile = UserProfile.objects.get(user=user)
-
-        # UserProfile이 존재할 경우 access_token 업데이트
-        user_profile.access_token = access_token
-        user_profile.save()  # 변경 사항 저장
-
-        return JsonResponse({
-          'user': {
-            'username': user.username,
-            'email': user.email,
-            'first_name': user.first_name,
-            'last_name': user.last_name,  # API에서 받아온 사용자 이름
-          }
-        })
-
-      # UserProfile이 없을 경우 2단계 인증 진행
-      except UserProfile.DoesNotExist:
-        request.session['user_id'] = user.id  # 사용자 ID를 세션에 저장
-        request.session['username'] = user.username  # 사용자 ID를 세션에 저장
-        request.session['email'] = user.email  # 이메일을 세션에 저장
-        request.session['first_name'] = user.first_name  # 이름을 세션에 저장
-        request.session['last_name'] = user.last_name  # 성을 세션에 저장
-        request.session['access_token'] = access_token  # access_token을 세션에 저장
-        return login_2fa(request, user)  # 2단계 인증 시작
-
-    else:
-      return JsonResponse({'error': 'Failed to retrieve access token'},
-                          status=400)
-
+def save_user_info_to_sessioin(request, user, access_token):
+  request.session['user_id'] = user.id  # 사용자 ID를 세션에 저장
+  request.session['username'] = user.username  # 사용자 ID를 세션에 저장
+  request.session['email'] = user.email  # 이메일을 세션에 저장
+  request.session['first_name'] = user.first_name  # 이름을 세션에 저장
+  request.session['last_name'] = user.last_name  # 성을 세션에 저장
+  request.session['access_token'] = access_token  # access_token을 세션에 저장
+# ----------------- 2FA -----------------
 
 # 2FA 시작 함수
 def login_2fa(request, user):
@@ -123,7 +139,6 @@ def login_2fa(request, user):
 
   # 세션에 코드 저장
   request.session['2fa_code'] = code  # 예시
-
   return redirect('verify_2fa_code')
 
 # 2FA 코드 전송 함수
@@ -186,7 +201,6 @@ def verify_2fa_code(request):
             'last_name': user.last_name,  # API에서 받아온 사용자 이름
           }
         })
-        # return JsonResponse({'success': '2FA verified!'}, status=200)
       else:
         # 코드가 일치하지 않는 경우
         return JsonResponse({'error': 'Invalid 2FA code'}, status=400)
